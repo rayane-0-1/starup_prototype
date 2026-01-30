d3addf4f9824fea7aedda0fe7b96156776532b9d
rayane-0-1 <tvrayane67@gmail.com>
2026-01-30 23:19:15 +0100

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,183 @@
+const navLinks = [
+ {
+	id: "cocktails",
+	title: "Cocktails",
+ },
+ {
+	id: "about",
+	title: "About Us",
+ },
+ {
+	id: "work",
+	title: "The Art",
+ },
+ {
+	id: "contact",
+	title: "Contact",
+ },
+];
+
+const cocktailLists = [
+ {
+	name: "Chapel Hill Shiraz",
+	country: "AU",
+	detail: "Battle",
+	price: "$10",
+ },
+ {
+	name: "Caten Malbee",
+	country: "AU",
+	detail: "Battle",
+	price: "$49",
+ },
+ {
+	name: "Rhino Pale Ale",
+	country: "CA",
+	detail: "750 ml",
+	price: "$20",
+ },
+ {
+	name: "Irish Guinness",
+	country: "IE",
+	detail: "600 ml",
+	price: "$29",
+ },
+];
+
+const mockTailLists = [
+ {
+	name: "Tropical Bloom",
+	country: "US",
+	detail: "Battle",
+	price: "$10",
+ },
+ {
+	name: "Passionfruit Mint",
+	country: "US",
+	detail: "Battle",
+	price: "$49",
+ },
+ {
+	name: "Citrus Glow",
+	country: "CA",
+	detail: "750 ml",
+	price: "$20",
+ },
+ {
+	name: "Lavender Fizz",
+	country: "IE",
+	detail: "600 ml",
+	price: "$29",
+ },
+];
+
+const profileLists = [
+ {
+	imgPath: "/images/profile1.png",
+ },
+ {
+	imgPath: "/images/profile2.png",
+ },
+ {
+	imgPath: "/images/profile3.png",
+ },
+ {
+	imgPath: "/images/profile4.png",
+ },
+];
+
+const featureLists = [
+ "Perfectly balanced blends",
+ "Garnished to perfection",
+ "Ice-cold every time",
+ "Expertly shaken & stirred",
+];
+
+const goodLists = [
+ "Handpicked ingredients",
+ "Signature techniques",
+ "Bartending artistry in action",
+ "Freshly muddled flavors",
+];
+
+const storeInfo = {
+ heading: "Where to Find Us",
+ address: "33.836181, -6.921924, Tamesna 12200",
+ contact: {
+	phone: "+2126 63 06 92 75",
+	email: "hello@starup.com",
+ },
+};
+
+const openingHours = [
+ { day: "Mon–Thu", time: "8:00am – 12am" },
+ { day: "Fri", time: "8:30am – 12am" },
+ { day: "Sat", time: "8:00am – 12am" },
+ { day: "Sun", time: "8:00am – 12am" },
+];
+
+const socials = [
+ {
+	name: "Instagram",
+	icon: "/images/insta.png",
+	url: "https://www.instagram.com/starupcoffee/",
+ },
+ {
+	name: "X (Twitter)",
+	icon: "/images/x.png",
+	url: "#",
+ },
+ {
+	name: "Facebook",
+	icon: "/images/fb.png",
+	url: "https://www.facebook.com/starupcoffeetamesna/",
+ },
+];
+
+const allCocktails = [
+ {
+	id: 1,
+	name: "Classic Mojito",
+	image: "/images/drink1.png",
+	title: "Simple Ingredients, Bold Flavor",
+	description:
+	 "Made with tequila, lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
+ },
+ {
+	id: 2,
+	name: "Raspberry Mojito",
+	image: "/images/drink2.png",
+	title: "A Zesty Classic That Never Fails",
+	description:
+	 "The Margarita is a classic that balances tangy lime, smooth tequila, and a touch of sweetness. Shaken, frozen, or on the rocks—it’s always crisp & refreshing.",
+ },
+ {
+	id: 3,
+	name: "Violet Breeze",
+	image: "/images/drink3.png",
+	title: "Simple Ingredients, Bold Flavor",
+	description:
+	 "Made with lime juice, and orange liqueur, the Margarita is easy to make and full of character. Add a salted rim for the perfect drink on summer nights.",
+ },
+ {
+	id: 4,
+	name: "Curacao Mojito",
+	image: "/images/drink4.png",
+	title: "Crafted With Care, Poured With Love",
+	description:
+	 "Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
+ },
+];
+
+export {
+ navLinks,
+ cocktailLists,
+ mockTailLists,
+ profileLists,
+ featureLists,
+ goodLists,
+ openingHours,
+ storeInfo,
+ socials,
+ allCocktails,
+};
